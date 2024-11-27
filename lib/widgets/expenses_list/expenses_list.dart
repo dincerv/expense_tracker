2be95eefbf599980379be1b3084b64aa5f44317d
Dincer Velioglu <dincer.velioglu@gmail.com>
2024-11-27 13:37:56 +0300

    feat: Enable expense deletion with Dismissible and Snackbar integration
    Added Dismissible widget in ExpensesList to enable swipe-to-delete functionality
    Implemented callback to remove dismissed expenses dynamically
    Integrated Snackbar to notify users upon expense deletion

--- lib/widgets/expenses_list/expenses_list.dart
+++ lib/widgets/expenses_list/expenses_list.dart
@@ -6,15 +6,25 @@ class ExpensesList extends StatelessWidget {
   const ExpensesList({
     super.key,
     required this.expenses,
+    required this.onRemoverExpense,
   });
 
   final List<Expense> expenses;
+  final void Function(Expense expense) onRemoverExpense;
 
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
       itemCount: expenses.length,
-      itemBuilder: (ctx, index) => ExpenseItem(expenses[index]),
+      itemBuilder: (ctx, index) => Dismissible(
+        key: ValueKey(expenses[index]),
+        onDismissed: (direction) {
+          onRemoverExpense(expenses[index]);
+        },
+        child: ExpenseItem(
+          expenses[index],
+        ),
+      ),
     );
   }
 }
